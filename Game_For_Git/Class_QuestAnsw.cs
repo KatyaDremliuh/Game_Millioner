bf259bb60e52745c61b9c81551d3696be98b0c1c
KatyaDremliuh <nerona.91@gmail.com>
2021-03-05 23:03:58 +0300

    Initial commit

---
+++ Game_For_Git/Class_QuestAnsw.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Game_Millioner
+{
+    public class QuestAnsw
+    {
+        public QuestAnsw(string question, string answ1, string answ2, string answ3, string answ4, int correctAnsw)
+        {
+            this.question = question; // это конструктор
+            this.answ1 = answ1;
+            this.answ2 = answ2;
+            this.answ3 = answ3;
+            this.answ4 = answ4;
+            this.correctAnsw = correctAnsw;
+        }
+
+        public string question; // это поля класса
+        public string answ1;
+        public string answ2;
+        public string answ3;
+        public string answ4;
+        public int correctAnsw;
+
+        public void Ask() // задать вопрос и дать 4 вар-та ответа
+        {
+            Console.WriteLine(question);
+            Console.WriteLine($"{answ1}\t{answ2}\n{answ3}\t{answ4}");
+        }
+
+        public uint GetAnsw(string userAnswer) // получить ответ от игрока
+        {
+            // userAnswer = Console.ReadLine();
+            uint userAnswerInt = Convert.ToUInt32(userAnswer);
+            return userAnswerInt;
+        }
+
+        public bool AnalyzeAnsw(uint userAnswInt) // проверить ответ на правильность и дать приз
+        {
+            bool result;
+
+            if (userAnswInt > 0 && userAnswInt < 5)
+            {
+                if (userAnswInt != correctAnsw)
+                {
+                    result = false;
+                    Console.WriteLine("Неверно. Вы покидаете игру ни с чем.");
+                }
+                else
+                {
+                    result = true;
+                }
+                return result;
+            }
+            else
+            {
+                if (userAnswInt > 5)
+                {
+                    Console.WriteLine("Введите ответ, используя только цифры 1, 2, 3 или 4");
+                    string repeatEnter = Console.ReadLine();
+                    userAnswInt = Convert.ToUInt32(repeatEnter);
+
+                }
+                return AnalyzeAnsw(userAnswInt);
+
+            }
+        }
+
+        public bool LetsMorePlay() // хочет ли продолжить игру
+        {
+            bool result;
+            Console.WriteLine($"Играем дальше? YES or NO?");
+            string play = Console.ReadLine();
+            if (play.ToLower() == "no")
+            {
+                result = false;
+            }
+            else
+            {
+                if (play.ToLower() == "yes")
+                {
+                    result = true;
+                }
+                else
+                {
+                    if (play.ToLower() != "yes" || play.ToLower() != "no")
+                    {
+                        Console.WriteLine("Введите корректно ответ.");
+                    }
+                    return LetsMorePlay();
+                }
+            }
+            return result;
+        }
+    }
+}
+
+
---
+++ Game_For_Git/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+
+namespace Game_Millioner
+{
+    class Program
+    {
+        static int IncreaseThePrize(int prize) // функция удвоение приза
+        {
+            return prize * 2;
+        }
+        static void Main(string[] args)
+        {
+            int prize = 100;
+            Console.WriteLine("Вас приветствует игра \"Кто хочет стать миллионером\". Вам будут заданы вопросы и" +
+            "предложены 4 варианта ответа, где правильный ответ только один. " +
+            "Первый правильный ответ приносит 100 BYN, каждый следующий верный ответ" +
+            "удваивает сумму выигрыша.");
+            Console.WriteLine("Введите Ваше имя.");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name}, начинаем игру!");
+            QuestAnsw[] questions = new QuestAnsw[5];
+            questions[0] = new QuestAnsw("Как называется жидкое косметическое средство для мытья волос?",
+            "1 - Шампунь", "2 - Мыло", "3 - Тряпка", "4 - Порошок", 1);
+            questions[1] = new QuestAnsw("Из скольких месяцев состоит год?",
+            "1 - Из 9", "2 - Из 12", "3 - Из 4", "4 - Не состоит", 2);
+            questions[2] = new QuestAnsw("Как называется подвидный холм песка в пустыне?",
+            "1 - Гора", "2 - Утёс", "3 - Скала", "4 - Дюна", 4);
+            questions[3] = new QuestAnsw("Как называется помещение в судне, где живут матросы?",
+            "1 - Дом", "2 - Квартира", "3 - Кубрик", "4 - Кабинет", 3);
+            questions[4] = new QuestAnsw("Какой прибор помогает изучать морское дно?",
+            "1 - Батискаф", "2 - Дирижабль", "3 - Телескоп", "4 - Луноход", 1);
+
+            for (int i = 0; i < questions.Length; i++)
+            {
+                questions[i].Ask(); // задали вопрос
+                string usersAnsw = Console.ReadLine();
+                uint userAnswInt = questions[i].GetAnsw(usersAnsw); // игрок ввел ответ
+                bool resultOfAnalyse = questions[i].AnalyzeAnsw(userAnswInt); // проверили ответ на правильность
+
+                if (resultOfAnalyse == false) // закончили задавать вопросы после неправильного ответа
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Верно! Вы получаете {prize} BYN");
+                    prize = IncreaseThePrize(prize);
+                }
+
+                if (i == questions.Length - 1) // задать последний вопрос
+                {
+                    Console.WriteLine($"{name}, вы ответили на все вопросы и уходите с призом в {prize / 2} BYN");
+                }
+                else
+                {
+                    bool resultOfPlaing = questions[i].LetsMorePlay(); // вызываем функцию "хочет ли продолжить игру"
+                    if (resultOfPlaing == false) // если не хочет - выгнать вон
+                    {
+                        Console.WriteLine($"Спасибо за игру. Вы уходите с {prize / 2} BYN");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
